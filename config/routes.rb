d8ad16e4c5ee1e066a211dd382db2c551d7513b8
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-02-16 16:59:02 +0000

    Api seed working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'cocktails#index'
-  resources :ingredients
-  resources :cocktails
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [ :new, :create ]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+cocktails = JSON.parse(open("http://www.thecocktaildb.com/api/json/v1/1/search.php?s=rum").read)
+
+cocktails["drinks"].each do |cocktail_data|
+  cocktail = Cocktail.create(name: cocktail_data["strDrink"])
+  cocktail_data.each do |key, value|
+    if key =~ /strIngredient/ && !value.chomp.strip.empty?
+      ingredient = Ingredient.create(name: key)
+      dose_key = key.sub(/strIngredient/, "strMeasure")
+      dose = Dose.new(description: cocktail_data[dose_key].chomp.strip)
+      dose.ingredient = ingredient
+      dose.cocktail = cocktail
+      dose.save
+    end
+  end
+end
+
+
+
+
